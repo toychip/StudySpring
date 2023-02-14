8c2bbb81956f2b9fc6f439149f5fe264ab0f7009
toychip <dlawnsgud427@naver.com>
2023-02-14 15:08:27 +0900

    02141500_DetachView

---
+++ src/main/java/MVC/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,20 @@
+package MVC.servlet.web.frontcontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+    // jsp로 이동하는 행위를 랜더링으로 칭함
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
