57da9ddb6dca1865404b9d36c68e86e3374e8e92
toychip <dlawnsgud427@naver.com>
2023-02-15 17:08:31 +0900

    02151700_SpringMemberControllerV1

---
+++ src/main/java/MVC/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,15 @@
+package MVC.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller // Component scan의 대상이되고, RequestMappingHandelerMapping의 두가지 역할을 모두 수행한다.
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process(){
+        return new ModelAndView("new-form");
+    }
+
+}
---
+++ src/main/java/MVC/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,9 @@
+package MVC.servlet.web.springmvc.v2;
+
+import MVC.servlet.domain.member.MemberRepository;
+
+public class SpringMemberControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+}
