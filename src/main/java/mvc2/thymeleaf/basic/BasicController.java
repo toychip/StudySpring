d91e56dd3ca82e111c6fd004f01101a23f16b829
toychip <dlawnsgud427@naver.com>
2023-02-22 17:20:31 +0900

    02221730_Set_Property_Value

--- src/main/java/mvc2/thymeleaf/basic/BasicController.java
+++ src/main/java/mvc2/thymeleaf/basic/BasicController.java
@@ -88,6 +88,11 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/operation";
     }
+
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
     @Data
     static class User{
         private String username;
