0faee70fee71de600a65af9d44bd3d2ef6f967a1
toychip <dlawnsgud427@naver.com>
2023-02-22 15:46:51 +0900

    02221530_Variable_And_Underlying_Objectes

--- src/main/java/mvc2/thymeleaf/basic/BasicController.java
+++ src/main/java/mvc2/thymeleaf/basic/BasicController.java
@@ -1,9 +1,17 @@
 package mvc2.thymeleaf.basic;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -19,5 +27,49 @@ public class BasicController {
     public String textUnescaped(Model model){
         model.addAttribute("data", "Hello <b>Thymeleaf!</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model){
+        User userA = new User("userA", 20);
+        User userB = new User("userB", 30);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session){
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")     // Spring Bean 생성
+    static class HelloBean{
+        public String hello(String data){
+            return "Hello" + data;
+        }
+    }
+
+    @Data
+    static class User{
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
