8786e581cd7011552cd3a8f930f59316274fda6d
toychip <dlawnsgud427@naver.com>
2023-02-25 20:27:43 +0900

    02252030_upgradeMVC2

---
+++ src/main/java/WAS/itemservice/TestDataInit.java
@@ -0,0 +1,26 @@
+package WAS.itemservice;
+
+
+import WAS.itemservice.domain.Item;
+import WAS.itemservice.domain.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
--- src/main/java/WAS/itemservice/web/form/FormItemController.java
+++ src/main/java/WAS/itemservice/web/form/FormItemController.java
@@ -1,10 +1,9 @@
-package WAS.itemservice.web.basic;
+package WAS.itemservice.web.form;
 
 
 import WAS.itemservice.domain.Item;
 import WAS.itemservice.domain.ItemRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,28 +13,28 @@ import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
-@RequestMapping("/basic/items")
+@RequestMapping("/form/items")
 @RequiredArgsConstructor
-public class BasicItemController {
+public class FormItemController {
     private final ItemRepository itemRepository;
 
     @GetMapping
     public String items(Model model){
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "basic/items";
+        return "form/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "basic/item";
+        return "form/item";
     }
 
     @GetMapping("/add")
     public String addForm(){
-        return "basic/addForm";
+        return "form/addForm";
     }
                 // --- 같은 url로 들어오더라도 get, post에 따라 호출되는 메서드가 다르게 설정 ---
 //    @PostMapping("/add")
@@ -51,7 +50,7 @@ public class BasicItemController {
 
         model.addAttribute("item", item);
         // model and view를 생각하자. view에 보내주는 것을 item 객체에 담아서 보낸다는 점을 생각하면된다.
-        return "basic/item";
+        return "form/item";
     }
 
 //    @PostMapping("/add")
@@ -69,28 +68,29 @@ public class BasicItemController {
 
 //        model.addAttribute("item", item); 자동 추가, 생략 가능
 
-        return "basic/item";
+        return "form/item";
     }
     //    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item) {
 //                                     ('item') 이것을 지우면 default 설정은
 //                                     클래스면 Item -> 앞글자만 소문자로 바꾼 item으로 바뀌어서 model에 담기게 된다.
         itemRepository.save(item);
-        return "basic/item";
+        return "form/item";
     }
     //    @PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);   // @ModelAttribute 또한 생략 가능.
-        return "basic/item";         // 단순한 타입들 String, int, Integer 가 들어오면 @RequestParam이 적용되고
+        return "form/item";         // 단순한 타입들 String, int, Integer 가 들어오면 @RequestParam이 적용되고
 //                                      객체가 들어오면 @ModelAttribute가 적용된다.
     }
 
 //    @PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
-        return "redirect:/basic/items/" + item.getId();
+        return "redirect:/form/items/" + item.getId();
 //                                          id를 그대로 사용하는 것은 매우 위험하므로 RedirectAttributes를 사용하는 것이 좋다.
     }
+
 
     @PostMapping("/add")
     public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
@@ -99,7 +99,7 @@ public class BasicItemController {
 //        ^에서 redirectAttributes.addAttribute 사용하여 아래 return에 itemId에 값을 넣은 것으로 치환해서 리턴한다.
         redirectAttributes.addAttribute("status", true);
         // ^는 쿼리 파라미터 ?status=true로 넘어가게 된다.
-        return "redirect:/basic/items/{itemId}";
+        return "redirect:/form/items/{itemId}";
     }
 
     // 상품 수정 폼
@@ -107,13 +107,13 @@ public class BasicItemController {
     public String editForm(@PathVariable Long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "basic/editForm";
+        return "form/editForm";
     }
 
     @PostMapping("/{itemId}/edit")       // Long <-> long 차이, Long은 null 가능, long은 불가능
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
         itemRepository.update(itemId, item);
-        return "redirect:/basic/items/{itemId}";
+        return "redirect:/form/items/{itemId}";
         // GET -> 상품 수정 폼
         // POST -> 상품 수정 처리
     }
