dbbcc9a19bdc1afa93cb0de1fdfb9a2a10be7125
toychip <dlawnsgud427@naver.com>
2023-02-25 19:47:40 +0900

    02252000_Template_Layout

--- src/main/java/mvc2/thymeleaf/basic/FragmentController.java
+++ src/main/java/mvc2/thymeleaf/basic/FragmentController.java
@@ -12,4 +12,16 @@ public class FragmentController {
     public String template(){
         return "template/fragment/fragmentMain";
     }
+
+    @GetMapping("layout")
+    public String layout(){
+        return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtend(){
+        return "template/layoutExtend/layoutExtendMain";
+    }
+
+
 }
