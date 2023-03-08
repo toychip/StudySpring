c9f25ae1b08283f0b7f587f98c9a60ae35b6522c
toychip <dlawnsgud427@naver.com>
2023-03-09 04:23:44 +0900

    03090430_DirectValidation

--- src/main/java/WAS/itemservice/web/form/FormItemController.java
+++ src/main/java/WAS/itemservice/web/form/FormItemController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -133,13 +134,45 @@ public class FormItemController {
 
 
     @PostMapping("/add")
-    public String addItem(Item itemId, RedirectAttributes redirectAttributes) {
+    public String addItem(Item item, RedirectAttributes redirectAttributes, Model model) {
 //        log.info("item.open={}", item.getOpen());
-//        log.info("item.regions={}", item.getRegions());
-//        log.info("item.itemType={}", item.getItemType());
 
-//        Item item = itemRepository.save(item);
-        Item item = itemRepository.save(itemId);
+        // Validation Error 보관
+        Map<String, String> errors = new HashMap<>();
+
+        // Validation Logic
+        if (!StringUtils.hasText(item.getItemName())){
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice()>1000000){
+            errors.put("price", "가격은 1,000 ~ 1,000,000까지 허용합니다. ");
+        }
+
+        if(item.getQuantity() == null || item.getQuantity()>=9999){
+            errors.put("quantity", "수량은 최대 9,999까지 허용합니다.");
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getItemName()!=null &&item.getQuantity() !=null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+            }
+        }
+
+        // 검증 실패하면 다시 입력 폼으로, 실패 로직
+        if(!errors.isEmpty()){  // error가 empty가 아니면  에러가 없음이 아니면 == 에러가 있으면
+            log.info("errors = {}", errors);
+            model.addAttribute("errors", errors);
+            return "validation/v1/addForm"; // model에 담아서 다시 보여줄거니까 담아서, 입력 폼 뷰로 돌려보냄
+        }
+
+        // 성공 로직
+        
+
+
+        Item savedItem = itemRepository.save(item);
 //        redirectAttributes.addAttribute("item", item);
         redirectAttributes.addAttribute("itemId", item.getId());
 //        ^에서 redirectAttributes.addAttribute 사용하여 아래 return에 itemId에 값을 넣은 것으로 치환해서 리턴한다.
