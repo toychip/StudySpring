ccf307f89094b3aa76ac62d8aa7a9456d4390fae
toychip <dlawnsgud427@naver.com>
2023-03-13 08:23:32 +0900

    03130800_ErrorCodeMessageProcessing2

--- src/main/java/WAS/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/WAS/itemservice/web/validation/ValidationItemControllerV2.java
@@ -157,11 +157,13 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")        // BindingResult -> Item에 Binding된 결과가 bindingResult에 담김 == 잘 안담기고 오류가 생긴다면 bindingresult에 담김
+    // @PostMapping("/add")        // BindingResult -> Item에 Binding된 결과가 bindingResult에 담김 == 잘 안담기고 오류가 생긴다면 bindingresult에 담김
 //    또한 bindingResult가 있으면 오류가 났을때 바로 400 페이지로 가는것이 아닌 어떠한 오류가 났는지 bindingResult에 담고 전달하며, 바로 오류페이지가 아닌 우선 컨트롤러가 호출이된다.
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 //        log.info("item.open={}", item.getOpen());
 
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
         // Validation Error 보관
 //        Map<String, String> errors = new HashMap<>(); [ BindingResult 사용 후 이것 사용 x]
 
@@ -190,6 +192,68 @@ public class ValidationItemControllerV2 {
             if(resultPrice < 10000){
 //                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
                 bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"},new Object[]{10000, resultPrice},null));
+                // Object는 넘어올 값이 없음. 실패할 확률도 없음
+            }
+        }
+
+        // 검증 실패하면 다시 입력 폼으로, 실패 로직
+//        if(!errors.isEmpty()){  // error가 empty가 아니면  에러가 없음이 아니면 == 에러가 있으면
+        if(bindingResult.hasErrors()){
+//            log.info("errors = {}", errors);
+            log.info("errors = {}", bindingResult);
+//            model.addAttribute("errors", errors); 이것을 담지 않아도 됨, bindingResult에 자동으로 담기기 때문
+            return "validation/v2/addForm"; // model에 담아서 다시 보여줄거니까 담아서, 입력 폼 뷰로 돌려보냄
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+//        redirectAttributes.addAttribute("item", item);
+        redirectAttributes.addAttribute("itemId", item.getId());
+//        ^에서 redirectAttributes.addAttribute 사용하여 아래 return에 itemId에 값을 넣은 것으로 치환해서 리턴한다.
+        redirectAttributes.addAttribute("status", true);
+        // ^는 쿼리 파라미터 ?status=true로 넘어가게 된다.
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")        // BindingResult -> Item에 Binding된 결과가 bindingResult에 담김 == 잘 안담기고 오류가 생긴다면 bindingresult에 담김
+//    또한 bindingResult가 있으면 오류가 났을때 바로 400 페이지로 가는것이 아닌 어떠한 오류가 났는지 bindingResult에 담고 전달하며, 바로 오류페이지가 아닌 우선 컨트롤러가 호출이된다.
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+//             ** BindingResult에 내장되어 있는 rejactValue와 reject 도입 **
+//        rejectValue - FieldError
+//        reject    -  objectValue
+
+
+        //        log.info("item.open={}", item.getOpen());
+
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
+        // Validation Error 보관
+//        Map<String, String> errors = new HashMap<>(); [ BindingResult 사용 후 이것 사용 x]
+
+        // Validation Logic
+        if (!StringUtils.hasText(item.getItemName())){
+            bindingResult.rejectValue("itemName", "required");
+//            메시지에 등록된 오류코드가 아님. 뒤에 나올 messageResolver를 위한 오류 코드
+//            일단 errorCode -> Object명 field명 조합해서 만들어준다고 생각하기.
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice()>1000000) {
+//            errors.put("price", "가격은 1,000 ~ 1,000,000까지 허용합니다. ");                                                                                             파라미터 변수 값
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        }
+        if(item.getQuantity() == null || item.getQuantity()>=9999){
+//            errors.put("quantity", "수량은 최대 9,999까지 허용합니다.");
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+
+        // 특정 필드가 아닌 복합 룰 검증
+//         특정 필드가 아니므로 FieldError 사용 불가능
+        if(item.getItemName()!=null &&item.getQuantity() !=null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+//                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
                 // Object는 넘어올 값이 없음. 실패할 확률도 없음
             }
         }
