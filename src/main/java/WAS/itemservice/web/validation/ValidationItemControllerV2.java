9b15f192ccb173bdbf30ae28220c10037db60f54
toychip <dlawnsgud427@naver.com>
2023-03-13 21:02:19 +0900

    03132100_@Validated

--- src/main/java/WAS/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/WAS/itemservice/web/validation/ValidationItemControllerV2.java
@@ -12,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -26,6 +28,12 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
     private final ItemRepository itemRepository;
     private final ItemValidator itemValidator;
+
+    @InitBinder
+    public void init(WebDataBinder dataBinder){ // 검증기
+//        컨트롤러가 호출될때마다 WebDataBinder가 새로 생성되어 호출된다. 누가 호출이 되던 검증기를 적용할 수 있게 된다.
+        dataBinder.addValidators(itemValidator);
+    }
 
     @GetMapping
     public String items(Model model){
@@ -281,10 +289,33 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         itemValidator.validate(item, bindingResult);
+
+        // 검증 실패하면 다시 입력 폼으로, 실패 로직
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+//        redirectAttributes.addAttribute("item", item);
+        redirectAttributes.addAttribute("itemId", item.getId());
+//        ^에서 redirectAttributes.addAttribute 사용하여 아래 return에 itemId에 값을 넣은 것으로 치환해서 리턴한다.
+        redirectAttributes.addAttribute("status", true);
+        // ^는 쿼리 파라미터 ?status=true로 넘어가게 된다.
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")    // Validated를 사용하므로써 item에 대한 검증을 자동으로 해줌, 검증 한 후 똑같이 bindingResult에 담겨있음
+//                          @Validated는 검증기를 실행하라는 애노테이션이다. 이게 있으면 앞서 WebDataBinder에 등록한 검증기를 찾아서 실행
+//                          여러 검증기를 등록한다면 어떤 검증기가 실행되어야할지 구분이 필요한대 이때 support가 사용된다.
+//                          여기서는 supports(Item.class) 호출되고, 결과가 'utre'임로 'ItemValidator'의 'validate()'가 호출된다.
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes, Model model) {
 
         // 검증 실패하면 다시 입력 폼으로, 실패 로직
         if(bindingResult.hasErrors()){
