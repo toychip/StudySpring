93e5f02a8e0b380e788c3b0dd74ac3a63ed52e33
toychip <dlawnsgud427@naver.com>
2023-03-23 00:53:50 +0900

    03230100_CookieSecurityIssues

--- src/main/java/WAS/itemservice/web/HomeController.java
+++ src/main/java/WAS/itemservice/web/HomeController.java
@@ -1,15 +1,53 @@
 package WAS.itemservice.web;
 
+import WAS.itemservice.domain.member.Member;
+import WAS.itemservice.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+    //    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model){ // 여기서 reqired = false 란 이 파라미터를 필수 값으로 받을 것인지에 대한 설정임.
+        if (memberId == null){
+            return "home";
+        }
+        //로그인
+        Member loginMember = memberRepository.findById(memberId);
+        if(loginMember == null) { // db에 없는 경우, 쿠키가 너무 오래됐거나 등
+            return "home";
+        }
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0); // 쿠키 시간 0으로 만들어서 종료시킴
+        response.addCookie(cookie);
     }
 }
--- src/main/java/WAS/itemservice/web/login/LoginController.java
+++ src/main/java/WAS/itemservice/web/login/LoginController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @RequiredArgsConstructor
 @Slf4j
@@ -24,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult){
+    public String login(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response){
 
         if (bindingResult.hasErrors()){
             return "login/loginForm";
@@ -37,6 +40,15 @@ public class LoginController {
             return "login/loginForm";
         }
 
+//        로그인 성공 처리
+
+//        쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
+        /*
+      로그인에 성공하면 쿠키를 생성하고 HttpServletResponse에 담는다. 쿠키 이름은 memberId이고,
+      같은 회원의 id를 미리 담아둔다. 웹 브라우저는 종료 전까지 회원의 'id'를 서버에 계속 보내줄 것이다.
+         */
         return "redirect:/";
     }
 
