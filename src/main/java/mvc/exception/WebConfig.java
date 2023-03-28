ecf964a976d6def6b6c95b36f288a0cab403f541
toychip <dlawnsgud427@naver.com>
2023-03-29 02:43:59 +0900

    03290245_ServletException-interceptor

--- src/main/java/mvc/exception/WebConfig.java
+++ src/main/java/mvc/exception/WebConfig.java
@@ -1,15 +1,27 @@
 package mvc.exception;
 
 import mvc.exception.filter.LogFilter;
+import mvc.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error"
+                ,"error-type/**");  // 오류 페이지 경로를 넣을 수 있음
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
 
@@ -19,4 +31,6 @@ public class WebConfig implements WebMvcConfigurer {
         filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);  // 이 필터는 Filter는 request 두 가지의 경우에 사용이 된다.
         return filterRegistrationBean;
     }
+
+
 }
