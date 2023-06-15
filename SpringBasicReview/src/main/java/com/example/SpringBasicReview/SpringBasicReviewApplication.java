6e2b86184cd9a39292e197e5bb4d3e9b02fe5371
toychip <dlawnsgud427@naver.com>
2023-06-15 19:05:27 +0900

    06151900_Spring 기본편 복습(1)프로젝트 생성

---
+++ SpringBasicReview/src/main/java/com/example/SpringBasicReview/SpringBasicReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBasicReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicReviewApplication.class, args);
+	}
+
+}
---
+++ SpringBasicReview/src/test/java/com/example/SpringBasicReview/SpringBasicReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBasicReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
