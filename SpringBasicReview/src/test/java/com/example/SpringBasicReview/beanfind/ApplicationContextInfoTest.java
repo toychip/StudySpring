2c4c781b858d290c6dd2203d3c318dfeb92a94c3
toychip <dlawnsgud427@naver.com>
2023-06-19 13:52:41 +0900

    06191400_Spring 기본편 복습(8)Spring 컨테이너에 등록된 Bean 조회하기

---
+++ SpringBasicReview/src/test/java/com/example/SpringBasicReview/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,41 @@
+package com.example.SpringBasicReview.beanfind;
+
+import com.example.SpringBasicReview.Appconfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(Appconfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);   // 타입 지정을 안햇기 때문에 object형
+            System.out.println("beanDefinitionName = " + beanDefinitionName + "\n object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        // Spring 내부 등록 빈이 아닌 개발한 빈만 출력보기
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+
+            //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+            //Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName + "\n object = " + bean);
+            }
+
+        }
+    }
+}
