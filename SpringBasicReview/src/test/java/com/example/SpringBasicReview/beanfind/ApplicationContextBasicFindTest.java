eccb2c5fcfa0e9b207f5e2020e10e6269d55087f
toychip <dlawnsgud427@naver.com>
2023-06-20 21:26:39 +0900

    06202130_Spring 기본편 복습(9)Spring Bean 조회 - 기본

---
+++ SpringBasicReview/src/test/java/com/example/SpringBasicReview/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,49 @@
+package com.example.SpringBasicReview.beanfind;
+
+import com.example.SpringBasicReview.Appconfig;
+import com.example.SpringBasicReview.service.MemberService;
+import com.example.SpringBasicReview.service.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(Appconfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+//        System.out.println("memberService = " + memberService);
+//        System.out.println("memberService.getClass() = " + memberService.getClass());
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+        // 구현에 의존.. 좋은 코드가 아님
+    void findBeanByName2() {
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 실패")
+    void findBeanByNameX() {
+//        MemberService xxxx = ac.getBean("xxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("xxxx", MemberService.class));  //junit
+    }
+}
--- SpringBasicReview/src/test/java/com/example/SpringBasicReview/beanfind/ApplicationContextInfoTest.java
+++ SpringBasicReview/src/test/java/com/example/SpringBasicReview/beanfind/ApplicationContextInfoTest.java
@@ -28,14 +28,12 @@ public class ApplicationContextInfoTest {
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
-
             //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
             //Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
             if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
                 Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("beanDefinitionName = " + beanDefinitionName + "\n object = " + bean);
             }
-
         }
     }
 }
