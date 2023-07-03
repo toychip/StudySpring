2f59f4652e638a1e7480aff13c6922560af12290
toychip <dlawnsgud427@naver.com>
2023-07-03 23:53:38 +0900

    07032345_Spring 기본편 복습(27)빈 생명주기-빈 등록 초기화, 소멸 메서드

--- SpringBasicReview/src/test/java/com/example/SpringBasicReview/lifecycle/NetworkClient.java
+++ SpringBasicReview/src/test/java/com/example/SpringBasicReview/lifecycle/NetworkClient.java
@@ -3,7 +3,8 @@ package com.example.SpringBasicReview.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+//public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -31,16 +32,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init(){
         // 의존관계 주입이 끝나면 호출
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         // 빈이 종료될 때
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
