ee220a7ffcb409ff315e86d228e0e0ab62ebd0ac
toychip <dlawnsgud427@naver.com>
2023-07-11 22:28:30 +0900

    07112200_Spring 기본편 복습(30)프로토타입 스코프-Provider로 문제해결

--- SpringBasicReview/src/test/java/com/example/SpringBasicReview/scope/SingletonWithprototypeTest1.java
+++ SpringBasicReview/src/test/java/com/example/SpringBasicReview/scope/SingletonWithprototypeTest1.java
@@ -1,16 +1,13 @@
 package com.example.SpringBasicReview.scope;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,20 +38,34 @@ public class SingletonWithprototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;  // 생성시점에 주입
+        
+        // 싱글톤 내에서 프로토타입을 생성해서 넘길때 매번 생성하길 원하므로 ObjectProvider 도입
+        // Spring의 ApplicationContext의 모든 기능이 아니라 스프링 컨테이너에서 prototypebean을 찾아서 반환해줌
+        // 찾아주는 기능만 제공
+
+//        Springframework에 의존
+        /*
+        private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        @Autowired
+        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
+        } */
+
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean(Provider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
-        public int logic(){
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
