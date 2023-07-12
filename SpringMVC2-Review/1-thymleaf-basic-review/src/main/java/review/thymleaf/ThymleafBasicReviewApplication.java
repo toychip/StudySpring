010e13da43c2cbdcbcb54eea4fae36e05a1df0f1
toychip <dlawnsgud427@naver.com>
2023-07-12 14:42:10 +0900

    07121400_Spring-MVC2-Review-(1)_thymeleaf-start

---
+++ SpringMVC2-Review/1-thymleaf-basic-review/src/main/java/review/thymleaf/ThymleafBasicReviewApplication.java
@@ -0,0 +1,13 @@
+package review.thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafBasicReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymleafBasicReviewApplication.class, args);
+	}
+
+}
---
+++ SpringMVC2-Review/1-thymleaf-basic-review/src/test/java/review/thymleaf/ThymleafBasicReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package review.thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafBasicReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
