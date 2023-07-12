da33a3d8b5963c3d100771d81ea71a15ce5ac1cb
toychip <dlawnsgud427@naver.com>
2023-07-12 15:49:12 +0900

    07121530_Spring-MVC2-Review-(3)_thymeleaf-SpringEL

--- SpringMVC2-Review/1-thymleaf-basic-review/src/main/java/review/thymleaf/BasicController.java
+++ SpringMVC2-Review/1-thymleaf-basic-review/src/main/java/review/thymleaf/BasicController.java
@@ -1,9 +1,17 @@
 package review.thymleaf;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -20,6 +28,43 @@ public class BasicController {
         model.addAttribute("data", "<b> Review thymleaf!! </b>");
         return "basic/text-unexcaped";
     }
-    
 
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = User.builder()
+                .username("userA")
+                .age(10)
+                .build();
+
+        User userB = User.builder()
+                .username("userB")
+                .age(20)
+                .build();
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put(userA.getUsername(), userA);
+        map.put(userB.getUsername(), userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        @Builder
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
 }
