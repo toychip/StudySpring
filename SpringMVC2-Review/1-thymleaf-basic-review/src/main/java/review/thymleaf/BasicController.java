710a71d34cbd460bf0eddd5166d9a49e9397a07e
toychip <dlawnsgud427@naver.com>
2023-07-13 19:18:58 +0900

    07131900_Spring-MVC2-Review-(4)_thymeleaf-DefaultObject

--- SpringMVC2-Review/1-thymleaf-basic-review/src/main/java/review/thymleaf/BasicController.java
+++ SpringMVC2-Review/1-thymleaf-basic-review/src/main/java/review/thymleaf/BasicController.java
@@ -3,11 +3,13 @@ package review.thymleaf;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,19 +20,19 @@ import java.util.Map;
 public class BasicController {
 
     @GetMapping("/text-basic")
-    public String textBasic(Model model) {
+    public String textBasic(Model model) {  // String 전달
         model.addAttribute("data", "Review thymleaf!!");
         return "basic/text-basic";
     }
 
-    @GetMapping("/text-unescaped")
+    @GetMapping("/text-unescaped")  // String html 태그 excaped
     public String textUnexcaped(Model model) {
         model.addAttribute("data", "<b> Review thymleaf!! </b>");
         return "basic/text-unexcaped";
     }
 
     @GetMapping("/variable")
-    public String variable(Model model) {
+    public String variable(Model model) {       // List<Map<user닉네임, user객체>>
         User userA = User.builder()
                 .username("userA")
                 .age(10)
@@ -55,6 +57,20 @@ public class BasicController {
 
         return "basic/variable";
     }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "First Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hi " + data;
+        }
+    }
+
 
     @Data
     static class User {
