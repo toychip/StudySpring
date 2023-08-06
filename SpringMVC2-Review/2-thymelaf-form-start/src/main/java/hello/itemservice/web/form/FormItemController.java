13502975002ca9407ee3aa5a4478f59adba18ced
toychip <dlawnsgud427@naver.com>
2023-08-06 13:01:23 +0900

    Feat: Spring-mvc2-exception 복습 프로젝트 생성

--- SpringMVC2-Review/2-thymelaf-form-start/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ SpringMVC2-Review/2-thymelaf-form-start/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -32,7 +32,9 @@ public class FormItemController {
     }
 
     @GetMapping("/add")
-    public String addForm() {
+    public String addForm(Model model) {
+
+        model.addAttribute("item", new Item());
         return "form/addForm";
     }
 
