0c5303847cd64e4ddbca802934c04d1d0d74412c
toychip <dlawnsgud427@naver.com>
2023-08-12 21:51:09 +0900

    Feat: Spring-mvc2-exception 서블릿 예외처리 - 오류 화면 제공

---
+++ SpringMVC2-Review/8-exception/src/main/java/review/exception/WebServerCustomizer.java
@@ -0,0 +1,24 @@
+package review.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+        //                                    RuntimeException 자식 예외들도 전부 포함
+
+        // 에러 페이지 등록
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}
---
+++ SpringMVC2-Review/8-exception/src/main/java/review/exception/servlet/ErrorPageController.java
@@ -0,0 +1,26 @@
+package review.exception.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Controller
+public class ErrorPageController {
+
+
+    @RequestMapping("/error-page/404")
+    public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
+        log.info("errorPage 404");
+        return "error-page/404";
+    }
+
+    @RequestMapping("/error-page/500")
+    public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
+        log.info("errorPage 500");
+        return "error-page/500";
+    }
+}
