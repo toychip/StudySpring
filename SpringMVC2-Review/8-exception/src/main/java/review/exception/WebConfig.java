315e77932691a5030c26f20b77b2377acb295d17
toychip <dlawnsgud427@naver.com>
2023-08-14 17:13:49 +0900

    Feat: Spring-mvc2-exception 서블릿 예외처리 - 인터셉터

--- SpringMVC2-Review/8-exception/src/main/java/review/exception/WebConfig.java
+++ SpringMVC2-Review/8-exception/src/main/java/review/exception/WebConfig.java
@@ -3,8 +3,10 @@ package review.exception;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import review.exception.filter.LogFilter;
+import review.exception.interceptor.LogInterceptor;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
@@ -13,7 +15,8 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    // Filter
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -21,9 +24,22 @@ public class WebConfig implements WebMvcConfigurer {
         filterRegistrationBean.addUrlPatterns("/*");
 
         // 아래의 2가지 경우에 필터가 호출이 된다.
-
         // 처음에는 REQUEST로, 그 후에 다시 오류가 났을 때는 DispatcherType이 ERROR로 나오게된다.
         filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
         return filterRegistrationBean;
+    }
+
+    // Interceptor
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error")
+                .excludePathPatterns("/error-page/**"); // 오류 페이지 경로
+                // 인터셉터는 DispatcherType 같은 것이 없다.
+
+
     }
 }
