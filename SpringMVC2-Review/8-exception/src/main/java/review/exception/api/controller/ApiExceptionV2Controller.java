641c6f273250fd53e73e41782b6768631c9f4df9
toychip <dlawnsgud427@naver.com>
2023-09-04 11:01:20 +0900

    Feat: Spring-mvc2-exception @ControllerAdvice

--- SpringMVC2-Review/8-exception/src/main/java/review/exception/api/controller/ApiExceptionV2Controller.java
+++ SpringMVC2-Review/8-exception/src/main/java/review/exception/api/controller/ApiExceptionV2Controller.java
@@ -13,28 +13,6 @@ import review.exception.exhandler.ErrorResult;
 @RestController
 public class ApiExceptionV2Controller {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandler(IllegalArgumentException e) {
-        log.error("[exceptionHandler] ex", e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
-
-    @ExceptionHandler(UserException.class)
-    public ResponseEntity<ErrorResult> userHandler(UserException e){
-        log.error("[exceptionHandler] ex", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-
-        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult exHandler(Exception e) {
-        log.error("[exceptionHandler] ex", e);
-        return new ErrorResult("EX", "내부 오류");
-    }
-
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable String id) {
         if (id.equals("ex")) {
