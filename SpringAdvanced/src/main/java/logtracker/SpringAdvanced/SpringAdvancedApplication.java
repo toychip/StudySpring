935490049672bf13474e3a66e871f9b962b698a1
toychip <dlawnsgud427@naver.com>
2024-02-05 16:38:47 +0900

    고급편/섹션1: 프로젝트 생성
    섹션1: 예제 만들기

---
+++ SpringAdvanced/src/main/java/logtracker/SpringAdvanced/SpringAdvancedApplication.java
@@ -0,0 +1,13 @@
+package logtracker.SpringAdvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdvancedApplication.class, args);
+	}
+
+}
---
+++ SpringAdvanced/src/test/java/logtracker/SpringAdvanced/SpringAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package logtracker.SpringAdvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
