6d91375b1ae9ae7118f03261f572cce1743d59ae
toychip <dlawnsgud427@naver.com>
2024-02-21 23:44:37 +0900

    고급편/섹션1: 예제 프로젝트 만들기 - V0
    섹션1: 예제 만들기

---
+++ SpringAdvanced/src/main/java/logtracker/SpringAdvanced/v0/OrderRepositoryV0.java
@@ -0,0 +1,28 @@
+package logtracker.SpringAdvanced.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        // 저장 로직
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생");
+        }
+
+        // 상품 저장하는데 1초 걸림
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ SpringAdvanced/src/main/java/logtracker/SpringAdvanced/v0/OrderServiceV0.java
@@ -0,0 +1,15 @@
+package logtracker.SpringAdvanced.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV0 {
+
+    private final OrderRepositoryV0 orderRepositoryV0;
+
+    public void orderItem(String itemID) {
+        orderRepositoryV0.save(itemID);
+    }
+}
