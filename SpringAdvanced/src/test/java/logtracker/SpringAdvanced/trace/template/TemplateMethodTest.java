3dca903e08849a3e9bd33479cc076e26bf908ce7
toychip <dlawnsgud427@naver.com>
2024-03-01 22:15:15 +0900

    고급편/섹션3: 템플릿 메서드 패턴 - 예제1 (#1)
    섹션3: 템플릿 메서드 패턴과 콜백 패턴

---
+++ SpringAdvanced/src/test/java/logtracker/SpringAdvanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,38 @@
+package logtracker.SpringAdvanced.trace.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+}
