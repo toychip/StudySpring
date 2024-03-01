ce3c0bbc4635a9d91f157782289271e83feed5fc
toychip <dlawnsgud427@naver.com>
2024-03-01 22:44:40 +0900

    고급편/섹션3: 템플릿 메서드 패턴 - 예제3 (#1)
    섹션3: 템플릿 메서드 패턴과 콜백 패턴

--- SpringAdvanced/src/test/java/logtracker/SpringAdvanced/trace/template/TemplateMethodTest.java
+++ SpringAdvanced/src/test/java/logtracker/SpringAdvanced/trace/template/TemplateMethodTest.java
@@ -59,4 +59,35 @@ public class TemplateMethodTest {
         AbstractTemplate template2 = new SubClassLogic2();
         template2.execute();
     }
+
+    @Test
+    void templateMethodV2() {
+        /*
+         * 익명 내부 클래스란?
+         * 위처럼 사용하려는 것의 단점은, 오버라이드한 클래스를 생성해줘야하는 단점이 있다.
+         * 이를 해결하기 위해 익명 내부 클래스를 사용한다.
+         *
+         * 한 번 더 복습하게 되는데,
+         * 추상 클래스 혹은 인터페이스에 있는 것을 생성함과 동시에 오버라이드 하는 것을 말함
+         */
+
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+
+        log.info("클래스 이름1={}", template1.getClass());   // 결과: TemplateMethodTest$1 - 테스트 내에서 생성했으므로 임의로 만들어줌
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
+        template2.execute();
+    }
 }
