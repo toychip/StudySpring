1cf9ab90f9984a724ea112576de886a44ea1df0d
toychip <dlawnsgud427@naver.com>
2024-03-05 19:21:26 +0900

    고급편/섹션4: 예제 프로젝트 만들기 v3 (#5)
    섹션4: 프록시 패턴과 데코레이터 패턴

---
+++ 스프링 로드맵5 고급편/proxy/src/main/java/hello/proxy/app/v3/OrderControllerV3.java
@@ -0,0 +1,25 @@
+package hello.proxy.app.v3;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderService;
+
+    @GetMapping("/v3/request")
+    public String request(final String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v3/no-log")
+    public String noLog() {
+        return "noLog";
+    }
+}
