6a7b6a83f8fd17fded52a357cf22f083ac0a99a0
toychip <dlawnsgud427@naver.com>
2024-03-06 11:58:13 +0900

    고급편/섹션4: 인터페이스 기반 - 적용 (#5)
    섹션4: 프록시 패턴과 데코레이터 패턴

--- 스프링 로드맵5 고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 스프링 로드맵5 고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,18 +2,28 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.interface_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
-@Import({AppV1Config.class, AppV2Config.class})
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 // 주의 config 하위 클래스 AppV1Config 등을 컴포넌트 스캔 대상에 두지 않기 위해 작업
 public class ProxyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProxyApplication.class, args);
+    }
+
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
     }
 
 }
