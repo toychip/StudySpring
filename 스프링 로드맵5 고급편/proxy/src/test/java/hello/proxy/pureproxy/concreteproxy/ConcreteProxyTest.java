f44baaa719ba77447a265ca7b5844a6a37f4770f
toychip <dlawnsgud427@naver.com>
2024-03-06 12:03:57 +0900

    고급편/섹션4: 구체 클래스 기반 프록시 - 예제1 (#5)
    섹션4: 프록시 패턴과 데코레이터 패턴

---
+++ 스프링 로드맵5 고급편/proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -0,0 +1,15 @@
+package hello.proxy.pureproxy.concreteproxy;
+
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import org.junit.jupiter.api.Test;
+
+public class ConcreteProxyTest {
+
+    @Test
+    void noProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient concreteClient = new ConcreteClient(concreteLogic);
+        concreteClient.execute();
+    }
+}
