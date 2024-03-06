79d507a401a9017d65f0f0243a7405b3df88987c
toychip <dlawnsgud427@naver.com>
2024-03-06 10:06:13 +0900

    고급편/섹션4: 프록시 패턴 - 예제 코드2 (#5)
    섹션4: 프록시 패턴과 데코레이터 패턴

--- 스프링 로드맵5 고급편/proxy/src/test/java/hello/proxy/pureproxy/proxy/code/RealSubject.java
+++ 스프링 로드맵5 고급편/proxy/src/test/java/hello/proxy/pureproxy/proxy/code/RealSubject.java
@@ -1,4 +1,4 @@
-package hello.proxy.pureproxy.code;
+package hello.proxy.pureproxy.proxy.code;
 
 import lombok.extern.slf4j.Slf4j;
 
--- 스프링 로드맵5 고급편/proxy/src/test/java/hello/proxy/pureproxy/proxy/code/Subject.java
+++ 스프링 로드맵5 고급편/proxy/src/test/java/hello/proxy/pureproxy/proxy/code/Subject.java
@@ -1,4 +1,4 @@
-package hello.proxy.pureproxy.code;
+package hello.proxy.pureproxy.proxy.code;
 
 public interface Subject {
     String operation();
