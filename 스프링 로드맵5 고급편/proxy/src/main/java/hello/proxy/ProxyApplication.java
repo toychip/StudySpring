ea2c9c238bdc911517db58b28a55a07932515625
toychip <dlawnsgud427@naver.com>
2024-03-06 12:37:13 +0900

    고급편/섹션4: 구체 클래스 기반 프록시 - 적용 (#5)
    섹션4: 프록시 패턴과 데코레이터 패턴

--- 스프링 로드맵5 고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 스프링 로드맵5 고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,7 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.concrete_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.interface_proxy.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -12,7 +13,8 @@ import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
 //@Import({AppV1Config.class, AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 // 주의 config 하위 클래스 AppV1Config 등을 컴포넌트 스캔 대상에 두지 않기 위해 작업
 public class ProxyApplication {
---
+++ 스프링 로드맵5 고급편/proxy/src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderServiceConcreteProxy.java
@@ -0,0 +1,33 @@
+package hello.proxy.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderServiceConcreteProxy extends OrderServiceV2 {
+
+    private final OrderServiceV2 target;
+    private final LogTrace logTrace;
+
+
+    public OrderServiceConcreteProxy(final OrderServiceV2 target, final LogTrace logTrace) {
+        super(null);
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public void orderItem(final String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderService.orderItem()");
+
+            // target 호출
+            target.orderItem("itemId");
+
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+        }
+    }
+}
