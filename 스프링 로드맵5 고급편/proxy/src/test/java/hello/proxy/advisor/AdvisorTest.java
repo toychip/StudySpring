6abc5cb3dc5cb5af329475cf9833effd559a84cd
toychip <dlawnsgud427@naver.com>
2024-03-12 23:39:28 +0900

    고급편/섹션6: 예제 코드1 - 어드바이저 (#12)
    섹션6: 스프링이 지원하는 프록시

---
+++ 스프링 로드맵5 고급편/proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,31 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+@Slf4j
+public class AdvisorTest {
+    @Test
+    void advisorTest1() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        /*
+            예제에서는 항상 참인 PointCut을 추가
+            Advisor = Advise1 + Pointcut1
+            proxyFactory는 advisor가 필수, 지금까지 advisor 없이 advise만 사용해도 됐던 이유는 Default를 제공하기 때문이다.
+            기본적으로 아래와 같이 new DefaultPointcutAdvisor(Pointcut.TRUE, new XxxAdvice()) 가 생성된다.
+        */
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+}
