105175c70addbc3f8912bed361386e1418b97e8c
toychip <dlawnsgud427@naver.com>
2024-03-13 23:12:35 +0900

    고급편/섹션6: 프록시 패턴 - 적용2 (#12)
    섹션6: 스프링이 지원하는 프록시

--- 스프링 로드맵5 고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 스프링 로드맵5 고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -7,6 +7,7 @@ import hello.proxy.config.v1_proxy.interface_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -20,7 +21,8 @@ import org.springframework.context.annotation.Import;
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
-@Import(ProxyFactoryConfigV1.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 // 주의 config 하위 클래스 AppV1Config 등을 컴포넌트 스캔 대상에 두지 않기 위해 작업
 public class ProxyApplication {
