f553ba8ecb76baf35b3c08a4a3490de5cf52fdac
toychip <dlawnsgud427@naver.com>
2024-03-14 12:11:23 +0900

    고급편/섹션7: 빈 후처리기 - 적용 (#14)
    섹션7: 빈 후처리기: Bean 등록 과정에 basePackage를 삽입, 빈 후처리기에서 경로 확인하여 변환

--- 스프링 로드맵5 고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 스프링 로드맵5 고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -8,6 +8,7 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -22,7 +23,8 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
 //@Import(ProxyFactoryConfigV1.class)
-@Import(ProxyFactoryConfigV2.class)
+//@Import(ProxyFactoryConfigV2.class)
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 // 주의 config 하위 클래스 AppV1Config 등을 컴포넌트 스캔 대상에 두지 않기 위해 작업
 public class ProxyApplication {
