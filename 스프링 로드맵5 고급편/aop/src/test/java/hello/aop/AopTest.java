9d2da349715acf97065e8b25309170a0d324c288
toychip <dlawnsgud427@naver.com>
2024-03-20 19:37:30 +0900

    고급편/섹션10: 스프링 AOP 구현1 - 시작 (#19)
    섹션10: 스프링 AOP 구현: Spring AOP - @Aspect 첫 사용

--- 스프링 로드맵5 고급편/aop/src/test/java/hello/aop/AopTest.java
+++ 스프링 로드맵5 고급편/aop/src/test/java/hello/aop/AopTest.java
@@ -2,15 +2,19 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
