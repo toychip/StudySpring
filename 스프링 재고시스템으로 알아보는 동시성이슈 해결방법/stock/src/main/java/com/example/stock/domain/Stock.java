03520d8e781681e8b792931556101ee61baeaf9c
toychip <dlawnsgud427@naver.com>
2024-06-17 11:31:56 +0900

    동시성이슈/섹션1: 프로젝트 생성

---
+++ 스프링 재고시스템으로 알아보는 동시성이슈 해결방법/stock/src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,38 @@
+package com.example.stock.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Stock {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+    private Long quantity;
+
+//    public Stock(final Long productId, final Long quantity) {
+//        this.productId = productId;
+//        this.quantity = quantity;
+//    }
+
+    public Stock(final Long productId, final Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("재고는 0개 미만이 될 수 없습니다.");
+        }
+        this.quantity -= quantity;
+    }
+}
