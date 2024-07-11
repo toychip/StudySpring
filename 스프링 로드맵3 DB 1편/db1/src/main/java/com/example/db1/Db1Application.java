907f3d5fccfd0fc8c1d4a37ede01ef34ece57411
toychip <dlawnsgud427@naver.com>
2024-07-11 20:17:12 +0900

    DB1편/섹션1: 프로젝트 생성 (#27)

---
+++ 스프링 로드맵3 DB 1편/db1/src/main/java/com/example/db1/Db1Application.java
@@ -0,0 +1,13 @@
+package com.example.db1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db1Application.class, args);
+	}
+
+}
