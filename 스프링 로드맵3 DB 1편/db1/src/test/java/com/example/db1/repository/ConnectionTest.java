f040aa0a2f7e8c20f0366216291222d95720e7de
toychip <dlawnsgud427@naver.com>
2024-07-12 14:09:05 +0900

    DB1편/섹션1: DataSource 예제1 - DriverManager (#27)
    매번 커넥션을 새로 만들던 것을 DriverManagerDataSource를 사용하여 1번만 연결, 설정과 사용을 분리

---
+++ 스프링 로드맵3 DB 1편/db1/src/test/java/com/example/db1/repository/ConnectionTest.java
@@ -0,0 +1,44 @@
+package com.example.db1.repository;
+
+import static com.example.db1.connection.ConnectionConst.PASSWORD;
+import static com.example.db1.connection.ConnectionConst.URL;
+import static com.example.db1.connection.ConnectionConst.USERNAME;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@SpringBootTest
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        // 서로 다른 커넥션
+        log.info("connection={}, class={}", connection1, connection1.getClass());
+        log.info("connection={}, class={}", connection2, connection2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        // DriverManagerDataSource - 항상 새로운 커넥션을 휙득
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection connection1 = dataSource.getConnection();
+        Connection connection2 = dataSource.getConnection();
+
+        log.info("connection={}, class={}", connection1, connection1.getClass());
+        log.info("connection={}, class={}", connection2, connection2.getClass());
+    }
+}
