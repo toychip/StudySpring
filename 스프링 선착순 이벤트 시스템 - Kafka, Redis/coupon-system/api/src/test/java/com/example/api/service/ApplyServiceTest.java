e13d57d899b513e00535fe03fd294322ff5511aa
toychip <dlawnsgud427@naver.com>
2024-10-23 03:02:32 +0900

    선착순 이벤트/섹션2: 문제점: RaceCondition 발생
    쿠폰 Project

--- 스프링 선착순 이벤트 시스템 - Kafka, Redis/coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ 스프링 선착순 이벤트 시스템 - Kafka, Redis/coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -3,6 +3,9 @@ package com.example.api.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.api.repository.CouponRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,5 +26,29 @@ class ApplyServiceTest {
         long count = couponRepository.count();
 
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    void 여러명응모() throws InterruptedException {
+        int threadCount = 1000;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
     }
 }
