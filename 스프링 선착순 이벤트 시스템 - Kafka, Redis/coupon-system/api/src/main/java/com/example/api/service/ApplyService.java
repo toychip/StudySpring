f9d60c9abf8a7e8bd1957c27160cbddaddb00cbe
toychip <dlawnsgud427@naver.com>
2024-10-23 04:42:40 +0900

    선착순 이벤트/섹션5: 발급가능한 쿠폰 개수를 1인당 1개로 제한
    요구사항 변경

--- 스프링 선착순 이벤트 시스템 - Kafka, Redis/coupon-system/api/src/main/java/com/example/api/service/ApplyService.java
+++ 스프링 선착순 이벤트 시스템 - Kafka, Redis/coupon-system/api/src/main/java/com/example/api/service/ApplyService.java
@@ -2,6 +2,7 @@ package com.example.api.service;
 
 import com.example.api.domain.Coupon;
 import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -15,15 +16,25 @@ public class ApplyService {
 
     private final CouponCreateProducer couponCreateProducer;
 
+    private final AppliedUserRepository appliedUserRepository;
+
     public ApplyService(final CouponRepository couponRepository, final CouponCountRepository couponCountRepository,
-                        final CouponCreateProducer couponCreateProducer) {
+                        final CouponCreateProducer couponCreateProducer,
+                        final AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
 //        long count = couponRepository.count();
+        Long apply = appliedUserRepository.add(userId);
+
+        if (apply != 1) {
+            return;
+        }
+
         Long count = couponCountRepository.increment();
 
         if (count > 100) {
