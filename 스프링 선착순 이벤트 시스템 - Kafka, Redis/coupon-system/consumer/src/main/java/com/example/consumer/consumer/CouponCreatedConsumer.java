1942eee791d608eaafa9a5a1def07db0468f031e
toychip <dlawnsgud427@naver.com>
2024-10-23 04:51:18 +0900

    선착순 이벤트/섹션6: 쿠폰을 발급하다가 에러가 발생한 경우
    시스템 개선

--- 스프링 선착순 이벤트 시스템 - Kafka, Redis/coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
+++ 스프링 선착순 이벤트 시스템 - Kafka, Redis/coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
@@ -1,7 +1,11 @@
 package com.example.consumer.consumer;
 
 import com.example.consumer.domain.Coupon;
+import com.example.consumer.domain.FailedEvent;
 import com.example.consumer.repository.CouponRepository;
+import com.example.consumer.repository.FailedEventRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +14,25 @@ public class CouponCreatedConsumer {
 
     private final CouponRepository couponRepository;
 
-    public CouponCreatedConsumer(final CouponRepository couponRepository) {
+    private final FailedEventRepository failedEventRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(CouponCreatedConsumer.class);
+
+    public CouponCreatedConsumer(final CouponRepository couponRepository,
+                                 final FailedEventRepository failedEventRepository) {
         this.couponRepository = couponRepository;
+        this.failedEventRepository = failedEventRepository;
     }
 
     @KafkaListener(topics = "coupon_create", groupId = "group_1")
     public void listener(Long userId) {
-        couponRepository.save(new Coupon(userId));
+        try {
+            couponRepository.save(new Coupon(userId));
+        } catch (Exception e) {
+            // 실무에선 아래와 같은 방법보다는 DLQ를 많이 사용함. Batch로 처리해도 무관
+            logger.error("failed to create coupon::{}", userId);
+            failedEventRepository.save(new FailedEvent(userId));
+        }
+
     }
 }
