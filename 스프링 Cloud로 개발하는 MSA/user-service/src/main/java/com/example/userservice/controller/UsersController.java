55a6b54d4273ee4051dedca6258e809c689d636e
toychip <dlawnsgud427@naver.com>
2024-12-14 22:48:15 +0900

    MSA/섹션5: Users Microservice - welcome() 메소드(#46)

---
+++ 스프링 Cloud로 개발하는 MSA/user-service/src/main/java/com/example/userservice/controller/UsersController.java
@@ -0,0 +1,27 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.controller.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UsersController {
+
+    private final Greeting greeting;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return greeting.getMessage();
+    }
+
+}
