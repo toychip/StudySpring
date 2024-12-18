0243e8e2a3b83ef318e893bfe5d6f5d8122b0f9a
toychip <dlawnsgud427@naver.com>
2024-12-18 12:47:15 +0900

    MSA/섹션6: Users Microservice와 Spring Cloud Gateway 연동

--- 스프링 Cloud로 개발하는 MSA/user-service/src/main/java/com/example/userservice/controller/UsersController.java
+++ 스프링 Cloud로 개발하는 MSA/user-service/src/main/java/com/example/userservice/controller/UsersController.java
@@ -8,7 +8,7 @@ import com.example.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,16 +18,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 @RequiredArgsConstructor
 public class UsersController {
 
     private final Greeting greeting;
     private final UserService userService;
+    private final Environment env;
 
     @GetMapping("/health_check")
     public String status() {
-        return "It's Working in User Service";
+        return String.format(
+                "It's Working in User Service on PORT %s",
+                env.getProperty("local.server.port")
+        );
     }
 
     @GetMapping("/welcome")
