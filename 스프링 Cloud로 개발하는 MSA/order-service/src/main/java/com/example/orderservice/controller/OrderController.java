94a8a01d6c9a3661f5fe33f053ce655abe158bb8
toychip <dlawnsgud427@naver.com>
2024-12-24 12:55:15 +0900

    MSA/섹션6: Orders Microservice - 기능 구현 2

---
+++ 스프링 Cloud로 개발하는 MSA/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,73 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.service.OrderService;
+import com.example.orderservice.vo.RequestOrder;
+import com.example.orderservice.vo.ResponseOrder;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order-service")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final Environment env;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format(
+                "It's Working in User Service on PORT %s",
+                env.getProperty("local.server.port")
+        );
+    }
+
+
+    @PostMapping("/{userId}/orders")
+    public ResponseEntity<ResponseOrder> createOrder(@PathVariable String userId,
+                                                     @RequestBody RequestOrder orderDetails) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        OrderDto orderDto = mapper.map(orderDetails, OrderDto.class);
+
+        orderDto.setUserId(userId);
+
+        OrderDto createdOrder = orderService.createOrder(orderDto);
+
+        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(responseOrder);
+    }
+
+    @GetMapping("/{userId}/orders")
+    public ResponseEntity<List<ResponseOrder>> createOrder(@PathVariable String userId) {
+        Iterable<OrderEntity> orderList = orderService.getOrdersByUserId(userId);
+        List<ResponseOrder> result = new ArrayList<>();
+
+        orderList.forEach(v -> {
+            result.add(new ModelMapper().map(v, ResponseOrder.class));
+        });
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(result);
+    }
+
+}
